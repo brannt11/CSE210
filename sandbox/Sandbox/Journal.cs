e130812918674134752cc965c2ddfacb183484eb
brannt11 <branntoluvsfootball@yahoo.com>
2024-03-09 18:42:38 -0700

    New Journal no bugs

--- sandbox/Sandbox/Journal.cs
+++ sandbox/Sandbox/Journal.cs
@@ -1,120 +1,76 @@
-public class Journal
-{
-    // variables
-    public List<JournalEntry> _journal = new List<JournalEntry>();
-    // private string fileName = "MyJournal.txt";
-    private string _userFileName;
+using System;
 
-
-    // method
-    public Journal()
+namespace classes{
+    public class Journal
     {
-    }
-
-    // A method that displays each journal entry
-    public void Display()
-    {
-        Console.WriteLine("\n************** Journal Entries **************");
-        foreach (JournalEntry journalEntry in _journal)
+        IDictionary<string, bool> prompts = new Dictionary<string, bool>();
+        public void AddKeyValuePair()
         {
-            journalEntry.Display();
+            prompts.Add("How was your day?", false);
+            prompts.Add("How is your family?", false);
+            prompts.Add("What was the weather like today?", false);
+            prompts.Add("What was fun today?", false);
+            prompts.Add("How was work today?", false);
+
         }
-        Console.WriteLine("\n******************** End ********************");
-    }
 
-    public void CreateJournalFile()
-    // Method to check if txt file is created if not create one
-    {
-        Console.Write("What your file name? ");
-        string userInput = Console.ReadLine();
-        _userFileName = userInput + ".txt";
+        public List<string> responses = new List<string>();
 
-        if (!File.Exists(_userFileName))
+        public void ShowMenu()
         {
-            File.CreateText(_userFileName);
-            Console.Write($"\n*** {_userFileName} has been created! ***\n");
-            Console.Write("***  Your journal entries have been saved. ***\n");
-            SaveJournalFile(_userFileName);
-            CreateJSON(userInput);
+            Console.WriteLine("Menu");
+            Console.WriteLine("Please Select one of the following options:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("What would you like to do?");
+
+            
         }
-        else
+
+        public void DisplayEntry()
         {
-            Console.Write($"\n*** {_userFileName} already exits. ***\n");
-            Console.Write("***  Your journal entries have been added. ***\n");
-            AppendJournalFile(_userFileName);
+            
+            responses.ForEach(Console.WriteLine);
         }
-    }
-    public void SaveJournalFile(string _userFileName)
-    // Method to save journal to txt file 
-    {
-        using (StreamWriter outputFile = new StreamWriter(_userFileName))
+
+        public void Write()
         {
-            foreach (JournalEntry journalEntry in _journal)
+            
+            
+            List<string> keys = new List<string>(prompts.Keys);
+            List<string> unansweredQuestion = new List<string>(keys.Where(question => this.prompts[question] == false));
+            string date = DateTime.Now.ToString("dd/MM/yyyy");
+            Random rndNum = new Random();
+            int listSize = unansweredQuestion.Count;
+            int rndIndex = rndNum.Next(0, listSize);
+            if (unansweredQuestion.Count == 0)
             {
-                outputFile.WriteLine($"{journalEntry._entryNumber}; {journalEntry._dateTime}; {journalEntry._journalPrompt}; {journalEntry._journalEntry}");
+                Console.WriteLine("No more prompts available");
+                return;
             }
-        }
-    }
+            string index  = rndIndex.ToString();
+            string randomQuestion = (unansweredQuestion[rndIndex]);
+            
+            prompts[randomQuestion] = true;
+            Console.WriteLine(randomQuestion);
+            
 
-    public void AppendJournalFile(string _userFileName)
-    // Method to save a new entry to journal txt file 
-    {
-        using (StreamWriter outputFile = new StreamWriter(_userFileName, append: true))
+            string answer = Console.ReadLine();
+            responses.Add($"Date: {date} - Prompt: {randomQuestion} - {answer}");
+
+        }
+
+        public Journal()
         {
-            foreach (JournalEntry journalEntry in _journal)
-            {
-                outputFile.WriteLine($"{journalEntry._entryNumber}; {journalEntry._dateTime}; {journalEntry._journalPrompt}; {journalEntry._journalEntry}");
-            }
+
         }
-    }
+            
+        
+            
+                
 
-    public void LoadJournalFile()
-    // Method to check if txt file is created and load it into the list.  
-    // This should replace any current info in the list
-    {
-        Console.Write("What your file name? ");
-        string userInput = Console.ReadLine();
-        _userFileName = userInput + ".txt";
-
-        if (File.Exists(_userFileName))
-        {
-            List<string> readText = File.ReadAllLines(_userFileName).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
-            foreach (string line in readText)
-            {
-                string[] entries = line.Split("; ");
-
-                JournalEntry entry = new JournalEntry();
-
-                entry._entryNumber = entries[0];
-                entry._dateTime = entries[1];
-                entry._journalPrompt = entries[2];
-                entry._journalEntry = entries[3];
-
-                _journal.Add(entry);
-            }
-        }
-    }
-
-
-    public void CreateJSON(string userInput)
-    // Method to create a JSON file 
-    {
-        string fileName = userInput + ".json";
-        List<JsonItem> _data = new List<JsonItem>();
-
-        foreach (JournalEntry journalEntry in _journal)
-        {
-            _data.Add(new JsonItem()
-            {
-                ID = journalEntry._entryNumber,
-                Date = journalEntry._dateTime,
-                Prompt = journalEntry._journalPrompt,
-                Entry = journalEntry._journalEntry
-            });
-        }
-
-        string json = JsonSerializer.Serialize(_data);
-        File.WriteAllText(fileName, json);
-    }
-
+    }  
 }
